357008b4cb06410ed981bb829c78aa5d99f33c67
srbsingh3 <32899793+srbsingh3@users.noreply.github.com>
2020-04-06 04:02:08 +0530

    to reduce the width

--- content.js
+++ content.js
@@ -3,3 +3,9 @@ const elements = document.querySelectorAll('._3j7s9');
 Array.from(elements).forEach((element) => {
       return element.remove()
   });
+
+const widths = document.querySelectorAll('._1FTCC');
+
+Array.from(widths).forEach((element) => {
+    return width.css('width', {$('._3FXB1').width})
+});
