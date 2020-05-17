d899fe34a3e9d9308f372860340216886ab7822a
srbsingh3 <32899793+srbsingh3@users.noreply.github.com>
2020-05-17 22:39:05 +0530

    Successfully hidden emojis in previews as well, but not in chats

--- inject.js
+++ inject.js
@@ -1,15 +1,22 @@
 (function() {
 	const names = document.querySelectorAll('._3NFp9');
+	const emojis = document.querySelectorAll('._3NFp9 .wa');
 
 	if(document.querySelector('._3NFp9').style.visibility == "") {
 		// Hide name and chat previews
 		Array.from(names).forEach((name) => {
 			return name.style.visibility = "hidden";
 			});
+		Array.from(emojis).forEach((emoji) => {
+			return emoji.style.visibility = "hidden";
+			});
 	} else{
 		// Show name and chat previews
 		Array.from(names).forEach((name) => {
 			return name.style.visibility = "";
+			});
+		Array.from(emojis).forEach((emoji) => {
+			return emoji.style.visibility = "";
 			});
 	}
 })();
