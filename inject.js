c1dc3b9258a287f5235a5df16eb6a6595b110821
srbsingh3 <32899793+srbsingh3@users.noreply.github.com>
2020-05-19 17:49:35 +0530

    Changed display-none to opacity-0

--- inject.js
+++ inject.js
@@ -2,21 +2,21 @@
 	const names = document.querySelectorAll('._3NFp9');
 	const emojis = document.querySelectorAll('._3NFp9 .wa');
 
-	if(document.querySelector('._3NFp9').style.display == "") {
+	if(document.querySelector('._3NFp9').style.opacity == "") {
 		// Hide name and chat previews
 		Array.from(names).forEach((name) => {
-			return name.style.display = "none";
+			return name.style.opacity = "0";
 			});
 		Array.from(emojis).forEach((emoji) => {
-			return emoji.style.display = "none";
+			return emoji.style.opacity = "0";
 			});
 	} else{
 		// Show name and chat previews
 		Array.from(names).forEach((name) => {
-			return name.style.display = "";
+			return name.style.opacity = "";
 			});
 		Array.from(emojis).forEach((emoji) => {
-			return emoji.style.display = "";
+			return emoji.style.opacity = "";
 			});
 	}
 })();
